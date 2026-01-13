6917eb9a1a7f638cce07a9692f03da7fcd25262b
Ian Gomez <11139566+iangomez@users.noreply.github.com>
2026-01-13 09:40:19 -0800

    Remove tap dance

--- keyboards/iandactyl/keymaps/default/keymap.c
+++ keyboards/iandactyl/keymaps/default/keymap.c
@@ -68,7 +68,7 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT_5x6(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                   KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
-    TD(TAB_ESC),    KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                   KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                   KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     ALT_F9,         LT1_A,          KC_S,           LT3_D,          LT4_F,          KC_G,                   KC_H,           KC_J,           KC_K,           KC_L,           LT3_CLN,        KC_QUOTE,       
     SFT_GRV,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                   KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLASH,       MAC_SS,   
                                     KC_NO,          KC_NO,                                                                                  KC_NO,          KC_NO, 
